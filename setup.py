917bead5c41bc3470e10ffc4529a2ab9eef36044
Daniel Nylander <daniel@danielnylander.se>
2026-03-17 09:17:41 +0100

    version: 0.2.0 (inte 1.0.0 så tidigt)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="grammarbuddy",
-    version="1.0.0",
+    version="0.2.0",
     description="Svensk grammatikträning med AI för SFI-nivå och lättläst svenska",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
